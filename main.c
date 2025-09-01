46c3833793bf3efa0013f0252efcec375ef4c35d
ShadKnow <142276800+ShadKnow@users.noreply.github.com>
2025-09-01 17:46:17 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char nama[50];
-    char bahasa[50];
-
-    printf("Masukkan Nama: ");
-    scanf("%s", nama);
-
-    printf("Masukkan Bahasa Pemrograman Favorit Anda: ");
-    scanf("%s", bahasa);
+    char nama[] = "Alvin";
+    char bahasa[] = "Computer Science";
 
     printf("Nama saya %s\n", nama);
     printf("Bahasa pemrograman favorit saya adalah %s\n", bahasa);
